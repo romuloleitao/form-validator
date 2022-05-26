aa541d574316f524ebc4907e4b58333301990aa7
Romulo Leitao <romulo.leitao@sogetel.com>
2022-05-26 12:27:08 -0400

    Added more input validations;

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function showError(input, message) {
     small.innerText = message;
 }
 
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return re.test(String(email).toLowerCase());
+}
+
 // Event listeners
 form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -20,5 +32,25 @@ form.addEventListener('submit', function (e) {
         showError(username, 'Username is required');
     } else {
         showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Email is not valid');
+    } else {
+        showSuccess(email);
+    }
+
+    if (password.value === '') {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Password 2 is required');
+    } else {
+        showSuccess(password2);
     }
 });
